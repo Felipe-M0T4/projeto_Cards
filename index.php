872b89094fd9c08757c730a46af111bb20dd5155
Felipe <>
2025-05-27 19:39:30 -0300

    atualizado

--- index.php
+++ index.php
@@ -4,22 +4,49 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
-    <link rel="icon" href="https://img.freepik.com/vetores-premium/icone-de-logotipo-de-vetor-do-instagram-logotipo-de-midia-social_901408-392.jpg?semt=ais_hybrid&w=740">
+    <link rel="stylesheet" href="./CSS/Login.css">
+    <link rel="icon" href="img/logo.png">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </head>
 <body>
-    <div class="conteiner-fluid login">
-         <p></p>
-     <img src="./outros//imagens//Instagram_files/E7m8ZCMOFDS.png" class="figure-img img-fluid">
-          
+    <div class="
+    container-fluid login justify-content-center d-flex align-items-center vh-100 flex-column">
+
+<div
+style="text-align: center; justify-content: center;">
+     <img style="width: 300px; height: 100px" src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/2560px-Instagram_logo.svg.png" class="figure-img img-fluid">
+    <br>      
+     <input type="email" placeholder="email:" class="mb-2 form-control">
+    <br><br>
+     <input type="password" placeholder="senha:" class="mb-2 form-control">
+    <br><br>
+     <button><a href="">entrar</a></button>
      <br>
-     <input type="text" >
+     <p>ou</p>
      <br>
-     <input type="text" >
-     <br>
-     <button>esqueceu a senha?</button>
-     <button>entrar</button>
-    </div>
+     <a href="">entrar com o facebook</a>
+    <br><br>
+     <a href="">esqueceu a senha?</button>
+    <br><br>
+     <p>Não tem conta?</p><a href="">Cadastre-se</a>
+</div>
+
+<div>
+    <a href="" >Meta</a>
+    <a href="">Sobre</a>
+    <a href="">Blog</a>
+    <a href="">Carreiras</a>
+    <a href="">Ajuda</a>
+    <a href="">API</a>
+    <a href="">Privacidade</a>
+    <a href="">Termos</a>
+    <a href="">Localizações</a>
+    <a href="">Instagram Lite</a>
+    <a href="">Threads</a>
+    <a href="">Uploads de contatos e não usuários</a>
+    <a href="">Meta Verified</a>
+</div>
 </body>
 </html>
-<style src="Login.css"></style>
